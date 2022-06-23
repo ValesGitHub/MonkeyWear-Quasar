728c92e77ad138cf4c2a72c8c52acaffb62bb99d
Alessandro Vandelli <a.vandelli@dreamonkey.com>
2022-06-23 17:38:54 +0200

    fix: Remanage routes and use dynamic pages

--- src/pages/CategoryProducts.vue
+++ src/pages/CategoryProducts.vue
@@ -1,27 +1,59 @@
 <template>
   <q-page>
     <h1>Ciao</h1>
+    {{ gender }}
+    {{ currentCategory }}
   </q-page>
 </template>
 
 <script setup lang="ts">
-import { defineComponent } from 'vue';
+import { computed, defineComponent, ref, watch } from 'vue';
 import { defineProps } from 'vue';
+import { useRoute } from 'vue-router';
 
 defineComponent({
   name: 'CategoryProducts',
 });
 
-const routerProps = defineProps({
+const route = useRoute();
+
+const props = defineProps({
   gender: {
     type: String,
     required: true,
   },
-  category: {
-    type: String,
-    required: true,
-  },
 });
+const isLoading = ref(true);
+const showError = ref(false);
+const products = ref([]);
+const currentCategory = computed(() => route.params.categoryName);
 
-console.log(routerProps.gender + routerProps.category);
+async function fetchData() {
+  isLoading.value = true;
+  try {
+    const { result, products: newProducts } = await getProducts(
+      props.gender,
+      currentCategory.value
+    );
+
+    if (result.success) {
+      products.value = newProducts;
+    } else {
+      products.value = [];
+      showError.value = true;
+    }
+
+    isLoading.value = false;
+  } catch (e) {
+    // Gestisci errore connessione
+  }
+}
+
+watch(
+  () => route.params,
+  () => {
+    fetchData();
+  },
+  { immediate: true }
+);
 </script>
